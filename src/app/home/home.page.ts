97caeea6a15421068f27203dfbf0a41eea7c7d0e
OrlaG25 <143258679+OrlaG25@users.noreply.github.com>
2025-01-05 20:45:22 +0000

    Third commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   standalone: true,
-  imports: [IonInput, IonHeader, IonToolbar, IonTitle, IonContent, FormsModule, IonButton],
+  imports: [IonInput, IonHeader, IonToolbar, IonTitle, IonContent, FormsModule, IonButton, IonIcon, RouterModule],
 })
 export class HomePage {
   countryName: string = ""
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+  }
 
   openCountries() {
     if (this.countryName.trim()) {
